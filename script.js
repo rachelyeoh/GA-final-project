56fb61374abe2d4904b17b30be4e8687c374848e
rachelyeoh <yeohrachel@gmail.com>
2022-06-29 13:13:02 +1000

    added validation for description and  assignee and display current date

--- script.js
+++ script.js
@@ -1,14 +1,64 @@
+/****************   Buttons   ****************/
+const createTaskButton = document.querySelector("#create-task")
+/****************   Input form   ****************/
+// Name
 const taskName = document.querySelector("#taskname");
 const taskNameInvalidFeedback = document.querySelector("#taskNameInvalidFeedback");
-const createTaskButton = document.querySelector("#create-task")
+// Description
+const descname = document.querySelector("#descname");
+const descnameInvalidFeedback = document.querySelector("#descnameInvalidFeedback");
+// Assignee
+const assigneename  = document.querySelector("#assigneename");
+const assigneenameInvalidFeedback = document.querySelector("#assigneenameInvalidFeedback");
+/****************   Date   ****************/
+// Current date
+let currentDateSpan = document.querySelector("#current-date");
+let todayDate = new Date();
+let date = `${todayDate.getDate()}/${todayDate.getMonth() + 1}/${todayDate.getFullYear()}`
+let todayDatePrimitive = todayDate[Symbol.toPrimitive]('number');
+// Due date
 
-createTaskButton.addEventListener("click", () => {
+
+
+const changeCurrentDate = () => {
+    currentDateSpan.innerText = date;
+}
+
+changeCurrentDate();
+
+const formValidation = () => {
     // If input length for name is less than or equal to 8
-    if(taskName.value.length <= 8) {
+     if(taskName.value.length <= 8) {
         taskNameInvalidFeedback.style.display = "inline";
         taskName.classList.add("is-invalid")
     } else {
         taskNameInvalidFeedback.style.display = "none";
         taskName.classList.remove("is-invalid")
     }
-})
+    // If input length for name is less than or equal to 15
+    if(descname.value.length <= 15) {
+       descnameInvalidFeedback.style.display = "inline";
+        descname.classList.add("is-invalid")
+    } else {
+        descnameInvalidFeedback.style.display = "none";
+        descname.classList.remove("is-invalid")
+    }
+    // If input length for name is less than or equal to 8
+    if(assigneename.value.length <= 8) {
+        assigneenameInvalidFeedback.style.display = "inline";
+         assigneename.classList.add("is-invalid")
+    } else {
+         assigneenameInvalidFeedback.style.display = "none";
+         assigneename.classList.remove("is-invalid")
+    }
+};
+
+
+
+createTaskButton.addEventListener("click", () => {
+    formValidation();
+    // let dueDate = document.querySelector("#duedate")
+    // console.log(dueDate.value)
+    // let dueDatePrimitive = dueDate[Symbol.toPrimitive]('number')
+    // console.log(dueDatePrimitive)
+});
