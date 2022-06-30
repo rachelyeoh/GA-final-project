c6bb9bc18573f5460146284cc0ad699093d1e141
rachelyeoh <yeohrachel@gmail.com>
2022-06-30 13:04:05 +1000

    move card to appropriate status category and select option show correct status

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ dueDate.forEach(dueDate => {
 
 /****************   Task Class/Object   ****************/
 let newTask = [];
+let tasksWithStatus = [];
 let insertNewTask;
 class TaskManager {
     constructor(name, description, assignedFName, assignedLName, dueDate, status) {
@@ -108,7 +109,7 @@ class TaskManager {
       this.assignedLName = assignedLName;
       this.dueDate = dueDate;
       this.status = status;
-      this.addTask();
+    //   this.addTask();
     }
 
     getAllTasks() {
@@ -116,10 +117,32 @@ class TaskManager {
     }
 
     getTasksWithStatus(status) {
-        console.log(JSON.parse(localStorage.getItem("Tasks"))) //assign variable + check if variable.status is status; return variable. 
+        let parsedTasksObject = JSON.parse(localStorage.getItem("Tasks"))
+        console.log(parsedTasksObject) //assign variable + check if variable.status is status; return variable. 
+        parsedTasksObject.forEach(eachTaskObject => {
+            console.log((eachTaskObject.status === status))
+            if((eachTaskObject.status === status)) {
+                return console.log(eachTaskObject)
+            } else {
+                return console.log("No Object Found")
+            }
+            
 
-    }
-    
+            
+            // if(status === "To do") {
+            //     console.log(eachTaskObject.status === "To do")
+            //     return eachTaskObject.status === "To do"
+            // } else if(status === "In Progress") {
+            //     console.log(eachTaskObject.status === status)
+            //     return eachTaskObject.status === "In Progress"
+            // } else if(status === "To review") {
+            //     return eachTaskObject.status === "To review"
+            // } else if(status === "Done") {
+            //     return eachTaskObject.status === "Done"
+            // };
+        });
+    };
+
     addTask() {
         newTask.push(this)
         localStorage.setItem("Tasks", JSON.stringify(newTask))
@@ -127,10 +150,15 @@ class TaskManager {
 };
 
 let taskCard;
+// let todoStatus;
+// let inProgressStatus;
+// let toReviewStatus;
+// let completedStatus;
 let todoContainer = document.querySelector('#to-do');
 let inProgressContainer = document.querySelector('#in-progress');
 let toReviewContainer = document.querySelector('#to-review');
 let completedContainer = document.querySelector('#completed');
+// let taskStatusContainer = document.querySelector('.status-container')
 
 
 
@@ -147,18 +175,61 @@ createTaskButton.addEventListener("submit", e => {
     let assigneeLastName = e.target.assigneeLName.value
     let dueDate = e.target.dueDate.value
     let status = e.target.status.value
+    let selectedOption
     taskCard = ''
     insertNewTask = new TaskManager(taskName, description, assigneeFirstName, assigneeLastName, dueDate, status)
-    // insertNewTask.addTask();
+    insertNewTask.addTask();
     insertNewTask.getAllTasks();
-    insertNewTask.getTasksWithStatus();
+    // insertNewTask.getTasksWithStatus("To do");
     e.target.reset()
     
-    
-
+    const displayCorrectStatus = () => {
+        if (status === "To do") {
+            return selectedOption = 
+            `<select class="status">
+                        <option class="bg-light" selected>To do</option>
+                        <option class="bg-warning">In Progress</option>
+                        <option class="bg-danger">To review</option>
+                        <option class="bg-success">Done</option>
+            </select>` 
+            // taskStatusContainer.innerHTML = todoStatus;
+        } else if(status === "In Progress") {
+            return selectedOption =
+            `<select class="status">
+                        <option class="bg-light">To do</option>
+                        <option class="bg-warning" selected>In Progress</option>
+                        <option class="bg-danger">To review</option>
+                        <option class="bg-success">Done</option>
+            </select>`
+            // taskStatusContainer.innerHTML = inProgressStatus;
+        } else if(status === "To review") {
+            return selectedOption =
+            `<select class="status">
+                        <option class="bg-light">To do</option>
+                        <option class="bg-warning">In Progress</option>
+                        <option class="bg-danger" selected>To review</option>
+                        <option class="bg-success">Done</option>
+            </select>` 
+            // taskStatusContainer.innerHTML = toReviewContainer;
+        } else if(status === "Done") {
+            return selectedOption =
+            `<select class="status">
+                        <option class="bg-light">To do</option>
+                        <option class="bg-warning">In Progress</option>
+                        <option class="bg-danger">To review</option>
+                        <option class="bg-success" selected>Done</option>
+            </select>` 
+            // taskStatusContainer.innerHTML = completedContainer;
+        }
+        
+    }
+    let newTaskCard;
+    // Create task
     const createTaskHTML = () => {
         newTask.forEach(task => {
-            taskCard += 
+            displayCorrectStatus();
+            newTaskCard = document.createElement("div");
+            taskCard = 
             `<div class="card shadow p-3 mb-2 bg-body rounded list-group-item">
                 <div class="card-body">
                 <div class="row">
@@ -166,13 +237,8 @@ createTaskButton.addEventListener("submit", e => {
                     <h5>${task.name}</h5>
                     <p>${task.description}</p>
                     </div>
-                    <div class="col-3">
-                    <select class="status">
-                        <option class="bg-light" selected>To do</option>
-                        <option class="bg-warning">In Progress</option>
-                        <option class="bg-danger">To review</option>
-                        <option class="bg-success">Done</option>
-                    </select>
+                    <div class="col-3 status-container">
+                    ${selectedOption}
                     </div>
                     <div class="row"></div>
                     <div class="col-5">
@@ -195,27 +261,66 @@ createTaskButton.addEventListener("submit", e => {
                 </div>
                 </div>
             </div>` 
+            newTaskCard.innerHTML = taskCard
     })};
     createTaskHTML();
 
+    // Display Task
     const render = () => {
         if (status === "To do") {
-            console.log("It's to do")
+            console.log(todoContainer)
+            todoContainer.insertAdjacentElement("beforeend", newTaskCard);
+            // todoStatus = 
+            // `<select class="status">
+            //             <option class="bg-light" selected>To do</option>
+            //             <option class="bg-warning">In Progress</option>
+            //             <option class="bg-danger">To review</option>
+            //             <option class="bg-success">Done</option>
+            // </select>` 
+            // taskStatusContainer.innerHTML = todoStatus;
         } else if(status === "In Progress") {
-            console.log("It's in progress")
+            inProgressContainer.insertAdjacentElement("beforeend", newTaskCard);
+            // inProgressContainer.innerHTML = taskCard;
+            // inProgressStatus = 
+            // `<select class="status">
+            //             <option class="bg-light">To do</option>
+            //             <option class="bg-warning" selected>In Progress</option>
+            //             <option class="bg-danger">To review</option>
+            //             <option class="bg-success">Done</option>
+            // </select>`
+            // taskStatusContainer.innerHTML = inProgressStatus;
         } else if(status === "To review") {
-            console.log("It's in review")
+            toReviewContainer.insertAdjacentElement("beforeend", newTaskCard);
+            // toReviewContainer.innerHTML = taskCard;
+            // toReviewContainer = 
+            // `<select class="status">
+            //             <option class="bg-light">To do</option>
+            //             <option class="bg-warning">In Progress</option>
+            //             <option class="bg-danger" selected>To review</option>
+            //             <option class="bg-success">Done</option>
+            // </select>` 
+            // taskStatusContainer.innerHTML = toReviewContainer;
         } else if(status === "Done") {
-            console.log("It's done")
+            completedContainer.insertAdjacentElement("beforeend", newTaskCard);
+            // completedContainer.innerHTML = taskCard;
+            // completedContainer = 
+            // `<select class="status">
+            //             <option class="bg-light">To do</option>
+            //             <option class="bg-warning">In Progress</option>
+            //             <option class="bg-danger">To review</option>
+            //             <option class="bg-success" selected>Done</option>
+            // </select>` 
+            // taskStatusContainer.innerHTML = completedContainer;
         }
-        todoContainer.innerHTML = taskCard;
+        
     }
     render();
-
+    
     
     
         
 });
+
 
 
 
